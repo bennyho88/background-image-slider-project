bd2b32b1f4f5ab3e7f7716b713b92d279a47b8a5
Benny Ho <benny.ho2530@gmail.com>
2020-02-29 19:05:39 +0100

    added btn function

--- js/app.js
+++ js/app.js
@@ -8,4 +8,42 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+  let counter = 0;
+
+  const container = document.querySelector('.img-container');
+  const btns = document.querySelectorAll('.btn');
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', function(event) {
+
+      let value = event.target;
+
+      if (value.classList.contains('btn-left')) {
+
+        counter--;
+        if(counter < 0 ) {
+          counter = pictures.length - 1
+        }
+
+        console.log(counter)
+
+        container.style.backgroundImage = `url(img/${pictures[counter]}.jpeg)`;
+      }
+
+      if(btn.classList.contains('btn-right')) {
+
+        counter++;
+
+        if( counter > pictures.length - 1) {
+          counter = 0;
+        }
+        
+        console.log(counter);
+
+        container.style.backgroundImage = `url(img/${pictures[counter]}.jpeg)`;
+      }
+
+    })
+  })
 })();
